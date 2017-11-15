b9f9308bf21b88363af1749d739cef0fa743cdc8
Lindsey <linrmz007@gmail.com>
2017-11-15 01:30:27 +0100

    result page is working

--- client/src/container/result.js
+++ client/src/container/result.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import NavBar from '../components/NavBar';
 import Paper from 'material-ui/Paper';
 import Avatar from 'material-ui/Avatar';
-import { Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const style = {
   height: 150,
@@ -27,38 +27,99 @@ const styles = {
   justifyContent: 'center'
 }
 
+const user = {
+  picture:'',
+  location:'',
+  name:'',
+  gender:'',
+  age:'',
+}
+
+
+// / always follow this procssess until you define yours
+// mock the data
+// render a view with mocked data (mock data should not be inline)
+// connect or fetch from the API
+// change mocked data to data from API
+// while  doing all check from time to time to see that your app is not broken
+
+//  defined resultIsANewArray = array.map((eachItemInArray, iterateeIndex) => {
+//    return AConstructedObjectFromEachItemInArray;
+//  });
+
 class Result extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      allMatches:[],
+    }
+
+    this.getRandomBuddies();
+  }
+
+  getBuddy = (data) => {
+    this.setState({
+      allMatches: this.state.allMatches.concat(data),
+    })
+  }
+
+  getRandomBuddies = (data) => {
+    fetch('https://randomuser.me/api/?results=10&nat=us', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(data => data.json())
+    .then(data => {
+      const matches = data.results.map(user => {
+        return {
+          picture: user.picture.large,
+          email: user.email
+        }
+      });
+      this.getBuddy(matches);
+    });
+  }
+
   render() {
+    this.getRandomBuddies();
+
     return (
       <div>
-        <NavBar/>
+      <NavBar/>
+        <div className='result-pg'>
           <div className='result-header'>
             <Paper styles={styles}
-            zDepth={2}><h5>User Persona Results</h5>
+            zDepth={2}><h5>87% Match!</h5>
             <p>Matches</p></Paper>
           </div>
-          <div className='match-made'>
-          <Paper style={style} zDepth={2} className="match-info">
-            <Avatar className='avatar-img' src="images/uxceo-128.jpg" />
 
-            <p>Similar travel buddies like you</p>
-            <div className="match-fb-btn">
-              <button className="button-primary">Connect via FB</button>
-            </div>
-          </Paper>
-          </div>
-          <div className='match-made'>
-          <Paper style={style} zDepth={2} className="match-info">
-            <Avatar className='avatar-img' src="images/uxceo-128.jpg" />
-            <p>Getin touch wit your purfect travel buddy</p>
-            <div className="match-fb-btn">
-              <button className="button-primary">Connect via FB</button>
-            </div>
-          </Paper>
+          {
+            this.state.allMatches.length > 0
+            ? this.state.allMatches.map((match, idx) => {
+              return (
+                  <div className='match-made' key={`match-${idx}`}>
+                    <Paper style={style} zDepth={2} className="match-info">
+                      <Avatar className='avatar-img' src={match.picture} />
+                      <p>Another ideal travel buddy match</p>
+
+                      <div className="match-fb-btn">
+                        <button className="button-primary">Connect via FB</button>
+                      </div>
+                    </Paper>
+                  </div>
+                )
+              })
+              : <Paper> Please wait we havent found a match!! </Paper>
+          }
+
           </div>
       </div>
     );
   }
 }
+
 
 export default Result;
